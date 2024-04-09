4b8b36f568dba12ea052eeccad8b0bf37876d589
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-04-09 17:15:14 -0400

    feat: add signup controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,51 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
     try {
-        const { fullName, username, password, confirmPassword } = req.body;
-    } catch (error) {
+        const { fullName, username, password, confirmPassword, gender } = req.body;
 
+        if (password !== confirmPassword) {
+            return res.status(400).json({ message: "Passwords do not match" });
+        }
+
+        const user = await User.findOne({ username });
+
+        if (user) {
+            return res.status(400).json({ message: "Username already exists" });
+        }
+
+        // Hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+        const otherProfilePic = `https://avatar.iran.liara.run/username?username=${fullName.split(" ").join("+")}`;
+
+        const newUser = new User({
+            fullName,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePicture: gender === "male" ? boyProfilePic : gender === "female" ? girlProfilePic : otherProfilePic
+        });
+
+        if (newUser) {
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePicture: newUser.profilePicture
+            });
+        } else {
+            res.status(400).json({ message: "Invalid user data" });
+        }
+
+    } catch (error) {
+        console.log(`Error in signup: ${error.message}`);
+        res.status(500).json({ message: error.message });
     }
 };
 
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config();
 
 import authRoutes from './routes/auth.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
+
+dotenv.config();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
 app.use('/api/auth', authRoutes);
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
 
 app.listen(PORT, () => {
     connectToMongoDB();
