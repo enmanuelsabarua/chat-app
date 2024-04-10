7a5fa7f969f423de53d3f69c69cdcca317602612
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-04-09 20:07:23 -0400

    fix: fix logout controller to clear JWT cookie and handle errors

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -65,6 +65,7 @@ export const login = async (req, res) => {
 
         // Generate JWT token
         generateTokenAndSetCookie(user._id, res);
+
         res.status(200).json({
             _id: user._id,
             fullName: user.fullName,
@@ -78,5 +79,11 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-    res.send('Logout');
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log(`Error in logout controller: ${error.message}`);
+        res.status(500).json({ message: error.message });
+    }
 }
