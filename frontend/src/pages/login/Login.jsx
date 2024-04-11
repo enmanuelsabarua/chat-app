114a3356b859c9b6f3b2fc65812f96a4a4231336
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-04-11 18:04:56 -0400

    feat: update login page UI

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,36 @@
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+            <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+                <h1 className="text-3xl font-semibold text-center text-gray-300">
+                    Login
+                    <span className="text-blue-500"> ChatApp</span>
+                </h1>
+
+                <form>
+                    <div>
+                        <label htmlFor="username" className="label p-2">
+                            <span className="text-base label-text">Username</span>
+                        </label>
+                        <input type="text" placeholder="Enter username" id="username" name="username" className="input input-bordered w-full h-10" />
+                    </div>
+
+                    <div>
+                        <label htmlFor="password" className="label p-2">
+                            <span className="text-base label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="Enter password" id="password" name="password" className="input input-bordered w-full h-10" />
+                    </div>
+
+                    <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-4 inline-block">{"Don't"} have an account? Signup</a>
+
+                    <div>
+                        <button className="btn btn-block btn-sm mt-4">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default Login;
