bb87aad245f5ef51196a045f5e500e62fb2f3f60
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-04-11 17:50:26 -0400

    feat: add login and signup pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Login from './pages/login/Login'
 
 function App() {
 
   return (
-    <div>
-      
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <Login />
     </div>
   )
 }
