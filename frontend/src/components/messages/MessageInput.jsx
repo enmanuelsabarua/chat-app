8de584aeb465bbe999d9c48249e6d802989285bd
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-04-14 16:17:56 -0400

    feat: update MessageInput component to use useSendMessage hook for sending messages

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import { BiSend } from "react-icons/bi";
+import useSendMessage from "../../hooks/useSendMessage";
 
 const MessageInput = () => {
+    const [message, setMessage] = useState('');
+    const { loading, sendMessage } = useSendMessage();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        if (!message.trim()) return;
+        await sendMessage(message);
+        setMessage('');
+    }
+
     return (
-        <form className="px-4 my-3">
+        <form className="px-4 my-3" onSubmit={handleSubmit}>
             <div className="w-full relative">
-                <input type="text" className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white" placeholder="Send a message" />
+                <input value={message} onChange={e => setMessage(e.target.value)} type="text" className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white" placeholder="Send a message" />
                 <button type="submit" className="absolute inset-y-0 end-0 flex items-center pe-3">
-                    <BiSend />
+                    {loading ? <div className="w-5 h-5 border-t-2 border-r-2 rounded-full border-blue-500 animate-spin"></div> : <BiSend />}
                 </button>
             </div>
         </form>
---
+++ frontend/src/hooks/useSendMessage.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import useConversation from '../zustand/useConversations';
+import { toast } from 'react-hot-toast';
+
+const useSendMessage = () => {
+    const [loading, setLoading] = useState(false);
+    const {messages, setMessages, selectedConversation} = useConversation();
+
+    const sendMessage = async message => {
+        setLoading(true);
+        try {
+            const res = await fetch(`/api/messages/send/${selectedConversation._id}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({message}),
+            });
+
+            const data = await res.json();
+
+            if (!res.ok) {
+                throw new Error(data.message);
+            }
+
+            setMessages([...messages, data]);
+        } catch (error) {
+            toast.error('Failed to send message');
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return {sendMessage, loading};
+}
+
+export default useSendMessage;
