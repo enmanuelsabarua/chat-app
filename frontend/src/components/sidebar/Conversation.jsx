a35eae5291b8ffe6c21e985cfc9f1d0d6b07025b
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2024-04-14 16:25:14 -0400

    feat: update UI styles in Conversation and SearchInput components

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -7,7 +7,7 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
     
     return (
         <>
-            <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer ${isSelected && "bg-sky-500"}`}
+            <div className={`flex gap-2 items-center transition hover:bg-blue-700 rounded p-2 py-1 cursor-pointer ${isSelected && "bg-blue-700"}`}
                 onClick={() => setSelectedConversation(conversation)}
             >
                 <div className="avatar online">
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -4,7 +4,7 @@ const SearchInput = () => {
     return (
         <form className="flex items-center gap-2">
             <input type="text" placeholder="Search" className="input input-bordered rounded-full" />
-            <button type="submit" className="btn btn-circle bg-sky-500 text-white">
+            <button type="submit" className="btn btn-circle bg-blue-700 text-white">
                 <IoSearchSharp className="w-6 h-6 outline-none" />
             </button>
         </form>
